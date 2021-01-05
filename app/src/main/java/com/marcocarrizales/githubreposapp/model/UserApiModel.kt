78d1f7fe22fa3d8c35eec42621ce2151c3a38b15
Marco Carrizales <5376705+tamoxin@users.noreply.github.com>
2021-01-04 21:26:04 -0800

    Revert "Working app. Need to redo the tests"
    This reverts commit d5a872187603edf0467dc6e05d7249a08cf34c69.

--- app/src/main/java/com/marcocarrizales/githubreposapp/model/UserApiModel.kt
+++
@@ -1,9 +0,0 @@
-package com.marcocarrizales.githubreposapp.model
-
-import com.squareup.moshi.Json
-
-data class UserApiModel(
-    val id: Long,
-    val login: String,
-    @field:Json(name = "avatar_url") val avatarUrl: String?
-)
